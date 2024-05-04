c93effc0d6a4fe2c66d1156f60cf4e52269ebed2
uniboxx <unibox@duck.com>
2024-05-04 23:28:09 +0200

    added dropdown

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const dropdown = document.querySelector('.dropdown');
-const dropWindow = document.querySelector(`.drop-window`);
+const dropItem = document.querySelector('.drop-item');
+const dropList = document.querySelector(`.drop-list`);
+const overlay = document.querySelector(`.overlay`);
+const nav = document.querySelector('nav');
 
-dropdown.addEventListener('click', e => {
-  dropWindow.classList.toggle('visible');
+dropItem.addEventListener('click', e => {
+  dropList.classList.toggle('visible');
+});
+[nav, overlay].forEach(el =>
+  el.addEventListener('click', e => {
+    if (
+      e.target.classList.contains('drop-item') ||
+      e.target.parentElement === dropList ||
+      e.target.parentElement === document.querySelector('.drop-container')
+    )
+      return;
+    dropList.classList.remove('visible');
+  })
+);
+
+dropList.addEventListener('click', e => {
+  const dropItem = e.target.closest('li');
+  if (!dropItem) return;
+  console.log(dropItem.textContent);
 });
