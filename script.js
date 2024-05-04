68bece0a3eb19cf8510584c4215ba5b514b879fc
uniboxx <unibox@duck.com>
2024-05-05 00:43:29 +0200

    fixed listener

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 const dropItem = document.querySelector('.drop-item');
 const dropList = document.querySelector(`.drop-list`);
-const overlay = document.querySelector(`.overlay`);
-const nav = document.querySelector('nav');
 
 dropItem.addEventListener('click', e => {
   dropList.classList.toggle('visible');
 });
-[nav, overlay].forEach(el =>
-  el.addEventListener('click', e => {
-    if (
-      e.target.classList.contains('drop-item') ||
-      e.target.parentElement === dropList ||
-      e.target.parentElement === document.querySelector('.drop-container')
-    )
-      return;
-    dropList.classList.remove('visible');
-  })
-);
+
+document.addEventListener('click', e => {
+  if (e.target === dropItem) return;
+  if (!dropList.contains(e.target)) dropList.classList.remove('visible');
+});
 
 dropList.addEventListener('click', e => {
   const dropItem = e.target.closest('li');
-  if (!dropItem) return;
   console.log(dropItem.textContent);
 });
